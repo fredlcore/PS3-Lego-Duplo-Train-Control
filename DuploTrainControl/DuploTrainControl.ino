6b05b9d445bdd6499eaad0eb688db988fc8e79d6
fredlcore <github@code-it.de>
2021-04-09 14:02:44 +0200

    Code cleanup and bugfixes

--- DuploTrainControl/DuploTrainControl.ino
+++ DuploTrainControl/DuploTrainControl.ino
@@ -24,57 +24,47 @@
 // create hub instances
 #define HUBS 3
 Lpf2Hub* myHubs[HUBS];
-Lpf2Hub* myHub;
 bool activeHubs[HUBS] = { false };
 int8_t activeHub = -1;
+int8_t speed[HUBS] = { 0 };
+int8_t parsed_speed[HUBS] = { 0 };
+int8_t saved_speed[HUBS] = { 0 };
 
-byte motorPort = (byte)DuploTrainHubPort::MOTOR;
-int speed[HUBS] = { 0 };
-bool analog_control[HUBS] = { 0 };
-int analog_speed = 0;
-int parsed_speed[HUBS] = { 0 };
-int old_parsed_speed[HUBS] = { 0 };
-int saved_speed[HUBS] = { 0 };
-int fillup_stage[HUBS] = { 0 };
+int8_t fillup_stage[HUBS] = { 0 };
 unsigned long fillup_timer[HUBS] = { 0 };
-int button = 0;
-int old_button = 0;
-char c = 0;
-unsigned long int idle_timer = millis();
-unsigned long int last_color_change = millis();
-int scanned_color = -1;
-Color LedColors[] = {BLACK, PINK, PURPLE, BLUE, LIGHTBLUE, CYAN, GREEN, YELLOW, ORANGE, RED, WHITE, NONE};
-byte Sounds[] = {(byte)DuploTrainBaseSound::BRAKE, (byte)DuploTrainBaseSound::STATION_DEPARTURE, (byte)DuploTrainBaseSound::WATER_REFILL, (byte)DuploTrainBaseSound::HORN, (byte)DuploTrainBaseSound::STEAM};
 
+// Color LedColors[] = {BLACK, PINK, PURPLE, BLUE, LIGHTBLUE, CYAN, GREEN, YELLOW, ORANGE, RED, WHITE, NONE};
+// byte Sounds[] = {(byte)DuploTrainBaseSound::BRAKE, (byte)DuploTrainBaseSound::STATION_DEPARTURE, (byte)DuploTrainBaseSound::WATER_REFILL, (byte)DuploTrainBaseSound::HORN, (byte)DuploTrainBaseSound::STEAM};
 
 void colorSensorCallback(void *hub, byte portNumber, DeviceType deviceType, uint8_t *pData) {
   Lpf2Hub *myHub = (Lpf2Hub *)hub;
+  static unsigned long last_color_change = millis();
 
   if (deviceType == DeviceType::DUPLO_TRAIN_BASE_COLOR_SENSOR && millis() - last_color_change > 3000) {
-    scanned_color = myHub->parseColor(pData);
-    delay(200);
     int8_t callback_hub = -1;
-    for (int i=0; i<HUBS; i++) {
+    for (int8_t i=0; i<HUBS; i++) {
       if (myHubs[i] == myHub) {
         callback_hub = i;
       }
     }
+    int8_t scanned_color = myHub->parseColor(pData);
     Serial.print("Scanned color: ");
     Serial.println(COLOR_STRING[scanned_color]);
     if (scanned_color == (byte)RED) {
       speed[callback_hub] = 0;
-      setSound(0, myHub);
+      setSound(DuploTrainBaseSound::BRAKE, myHub);
     } else if (scanned_color == (byte)BLUE) {
       fillup_stage[callback_hub] = 1;
       saved_speed[callback_hub] = speed[callback_hub];
       fillup_timer[callback_hub] = millis();
       speed[callback_hub] = 0;
     } else if (scanned_color == (byte)YELLOW) {
-      setSound(3, myHub);
+      setSound(DuploTrainBaseSound::HORN, myHub);
     } else if (scanned_color == (byte)GREEN) {
       speed[callback_hub] = -speed[callback_hub];
-      setSound(1, myHub);
-      test
+      setSound(DuploTrainBaseSound::STATION_DEPARTURE, myHub);
+    } else if (scanned_color == (byte)WHITE) {
+      setColor(WHITE, myHub);
     }
     last_color_change = millis();
   }
@@ -82,9 +72,11 @@ void colorSensorCallback(void *hub, byte portNumber, DeviceType deviceType, uint
 
 void speedometerSensorCallback(void *hub, byte portNumber, DeviceType deviceType, uint8_t *pData) {
   Lpf2Hub *myHub = (Lpf2Hub *)hub;
+  static int8_t old_parsed_speed[HUBS] = { 0 };
+
 
   if (deviceType == DeviceType::DUPLO_TRAIN_BASE_SPEEDOMETER) {
-    for (int i=0; i<HUBS; i++) {
+    for (int8_t i=0; i<HUBS; i++) {
       if (myHubs[i] == myHub) {
         old_parsed_speed[i] = parsed_speed[i];
         parsed_speed[i] = myHub->parseSpeedometer(pData);
@@ -97,36 +89,35 @@ void speedometerSensorCallback(void *hub, byte portNumber, DeviceType deviceType
 }
 
 // Change speed
-void move(int speed, Lpf2Hub *hub) {
+void move(int8_t speed, Lpf2Hub *hub) {
   Lpf2Hub *myHub = (Lpf2Hub *)hub;
   speed = (speed / 3) * 2;
   if (abs(speed) < 10) speed = 0;
   if (speed < -78) speed = -78;
   if (speed > 79) speed = 79;
   if (speed == 0 && parsed_speed[activeHub] == 0) {} else {
-      Serial.print(speed);
-    myHub->setBasicMotorSpeed(motorPort, speed);
+    myHub->setBasicMotorSpeed((byte)DuploTrainHubPort::MOTOR, speed);
   }
-  delay(50);
+//  delay(50);
 }
 
 // Play sound
-void setSound(byte sound, Lpf2Hub *hub) {
+void setSound(DuploTrainBaseSound sound, Lpf2Hub *hub) {
   Lpf2Hub *myHub = (Lpf2Hub *)hub;
   Serial.print("Sound: ");
-  Serial.println(sound);
-  for (int x=0; x<10; x++) {
-    myHub->playSound(Sounds[sound]);
+  Serial.println((byte)sound);
+  for (int8_t x=0; x<10; x++) {
+    myHub->playSound((byte)sound);
   }
 }
 
 // Change color
-void setColor(byte color, Lpf2Hub *hub) {
+void setColor(Color color, Lpf2Hub *hub) {
   Lpf2Hub *myHub = (Lpf2Hub *)hub;
   Serial.print("Color: ");
   Serial.println(COLOR_STRING[color]);
-  for (int x=0; x<10; x++) {
-    myHub->setLedColor(LedColors[color]);
+  for (int8_t x=0; x<10; x++) {
+    myHub->setLedColor(color);
   }
 }
 
@@ -143,14 +134,14 @@ int8_t setActiveHub(char c) {
 }
 
 int8_t nextHub(int8_t hub) {
-  setColor(5, myHubs[hub]);
+  setColor(CYAN, myHubs[hub]);
   int8_t i = 0;
-  for (int x=0; x<HUBS; x++) {
+  for (int8_t x=0; x<HUBS; x++) {
     i++;
     if (hub + i >= HUBS) i = -hub;
     if (activeHubs[hub+i] == true) {
       delay(200);
-      setColor(9, myHubs[hub+i]);
+      setColor(RED, myHubs[hub+i]);
       return hub+i;
     }
   }
@@ -166,15 +157,15 @@ void shutdown(int8_t hub) {
 void processFillup(int8_t hub) {  
   delay(50);
   if (fillup_stage[hub] == 1) {
-    setSound(4, myHub);
+    setSound(DuploTrainBaseSound::STEAM, myHubs[hub]);
     fillup_stage[hub] = 2;
   }
-  if (fillup_stage[hub] == 2 && millis() - fillup_timer[hub] > 2000) {
+  if (fillup_stage[hub] >= 2 && fillup_stage[hub] <= 4 && millis() - fillup_timer[hub] > 1300) {
     fillup_timer[hub] = millis();
-    setSound(2, myHub);
-    fillup_stage[hub] = 3;
+    setSound(DuploTrainBaseSound::WATER_REFILL, myHubs[hub]);
+    fillup_stage[hub] += 1;
   }
-  if (fillup_stage[hub] == 3 && millis() - fillup_timer[hub] > 4000) {
+  if (fillup_stage[hub] == 5 && millis() - fillup_timer[hub] > 2000) {
     speed[hub] = saved_speed[hub];
     fillup_stage[hub] = 0;
   }
@@ -185,7 +176,7 @@ void setup() {
   Serial.begin(115200);
   Serial1.begin(115200, SERIAL_8N1, 16, 17);
   Serial.print("Initializing...");
-  for (int x=0; x<HUBS; x++) {
+  for (int8_t x=0; x<HUBS; x++) {
     myHubs[x] = new Lpf2Hub;
     myHubs[x]->init();
   }
@@ -200,9 +191,17 @@ void setup() {
 
 // main loop
 void loop() {
+
+  static char c = 0;
+  static int8_t analog_speed = 0;
+  static bool analog_control[HUBS] = { 0 };
+  static uint8_t button = 0;
+  static uint8_t old_button = 0;
+  static unsigned long idle_timer = millis();
+
   // connect flow
-  for (int hub=0; hub<HUBS; hub++) {
-    myHub = myHubs[hub];
+  for (int8_t hub=0; hub<HUBS; hub++) {
+    Lpf2Hub* myHub = myHubs[hub];
     if (myHub->isConnecting()) {
       Serial.print("Connecting slot ");
       Serial.print(hub);
@@ -223,7 +222,7 @@ void loop() {
         // connect speed sensor and activate it for updates
         myHub->activatePortDevice((byte)DuploTrainHubPort::COLOR, colorSensorCallback);
         delay(200);
-        setSound(3, myHub);
+        setSound(DuploTrainBaseSound::HORN, myHub);
       } else {
         Serial.println("Failed to connect to Duplo Hub");
         myHub->init();
@@ -231,9 +230,9 @@ void loop() {
     }
   }
 
-  for (int hub=0; hub<HUBS; hub++) {
+  for (int8_t hub=0; hub<HUBS; hub++) {
 
-    myHub = myHubs[hub];
+    Lpf2Hub* myHub = myHubs[hub];
     if (myHub->isConnected()) {
 
       // Read keys from serial input
@@ -283,10 +282,10 @@ void loop() {
       if (button != old_button || button >= 64) { // Normally, only react if there has been a change, only for digital keypad up/down react always to allow for smooth increase of speed
         old_button = button;
         switch (button) {
-          case 1: setSound(1, myHubs[activeHub]); break;  // Station departure sound
-          case 2: setSound(4, myHubs[activeHub]); break;  // Steam sound
-          case 4: speed[activeHub] = 0; setSound(0, myHubs[activeHub]); break;        // Brake sound and stop train
-          case 8: setSound(3, myHubs[activeHub]); break;  // Horn sound
+          case 1: setSound(DuploTrainBaseSound::STATION_DEPARTURE, myHubs[activeHub]); break;
+          case 2: setSound(DuploTrainBaseSound::STEAM, myHubs[activeHub]); break;
+          case 4: speed[activeHub] = 0; setSound(DuploTrainBaseSound::BRAKE, myHubs[activeHub]); break;
+          case 8: setSound(DuploTrainBaseSound::HORN, myHubs[activeHub]); break;
           case 16: activeHub = nextHub(activeHub); break; // Select button pressed, switch active hub
           case 32: speed[activeHub] = 40; break;          // Start button pressed, set speed to 50% forward
           case 48: shutdown(activeHub); break;            // Start and Select button pressed, turn off train
@@ -323,7 +322,7 @@ void loop() {
 
   if (millis() - idle_timer > 300000 && activeHub > -1) {
     Serial.println("Idle time exceeded, shutting down hubs...");
-    for (int i=0; i<HUBS; i++) {
+    for (int8_t i=0; i<HUBS; i++) {
       if (myHubs[i]->isConnected()) {
         Serial.println(i);
         shutdown(i);
